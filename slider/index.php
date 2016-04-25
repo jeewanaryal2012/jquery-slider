1941aa787534f67ace5cbe678b4d0374d1792d63
jeewanaryal2012 <jeewan.developer@gmail.com>
2016-04-24 23:22:52 -0500

    changes

---
+++ slider/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title>jQuery Slider - By Jeewan</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<script src="https://code.jquery.com/jquery-1.11.3.js"></script>
+		<script src="js/main.js"></script>
+	</head>
+<body>
+	<div class="demo-slider">
+	  <div class="slider-wrapper">
+	    <div class="slider">
+	      <div class="slide">
+	      	<img src="images/c1.png">
+	      	<p>Title 1</p>
+	      </div>
+	      <div class="slide">
+	      	<img src="images/c2.png">
+			<p>Title 2</p>
+	      </div>
+	      <div class="slide">
+	      	<img src="images/c3.png">
+	      	<p>Title 3</p>
+	      </div>
+	      <div class="slide">
+	      	<img src="images/c4.png">
+	      	<p>Title 4</p>
+	      </div>
+	      <div class="slide">
+	      	<img src="images/c5.png">
+	      	<p>Title 5</p>
+	      </div>
+	      <div class="slide">
+	      	<img src="images/c6.png">
+	      	<p>Title 6</p>
+	      </div>
+	      <div class="slide">
+	      	<img src="images/c7.png">
+	      	<p>Title 7</p>
+	      </div>
+	      <div class="slide">
+	      	<img src="images/c8.png">
+	      	<p>Title 8</p>
+	      </div>
+	  </div>
+	  </div>
+	  
+	  <div class="btn-wrapper">
+	  	<input type="button" value="PREV" id="btn-prev" />
+	  	<input type="button" value="NEXT" id="btn-next" />
+	  </div>
+	</div>
+</body>
+</html>
---
+++ slider/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  var sliderCount = $(".slide").length;
+  $(".slider").width(sliderCount*200);
+  var l = 0;
+  //var cnt = $(".slide").length;
+  var slideWidth = $(".slide").width();
+  var cnt = 0;
+
+  $(document).on("click", "#btn-prev", function() {
+    if(cnt > 0) {
+      l = l + slideWidth;
+      $(".slider").animate({
+        "margin-left": l+"px"
+      }, 500, function() {});
+      cnt = cnt-1;
+    }
+    console.log(cnt);
+  });
+
+  $(document).on("click", "#btn-next", function() {
+    if(cnt < $(".slide").length-1) {
+      l = l - slideWidth;
+      $(".slider").animate({
+        "margin-left": l+"px"
+      }, 500, function() {});
+      cnt = cnt+1;
+    }
+    console.log(cnt);
+  });
+  
+});
